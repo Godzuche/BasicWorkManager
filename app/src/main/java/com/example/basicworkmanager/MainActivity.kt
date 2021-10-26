31435c7a9815abebbedc2bfa9196d25c32a7da9f
Administrator <godxuche@gmail.com>
2021-10-26 07:11:12 +0100

    created a WorkManager instance, a WorkRequest and input a workData into the SimpleWorker class through  the WorkRequest. Also created a OneTimeWorkRequest and a PeriodicWorkRequest...

--- app/src/main/java/com/example/basicworkmanager/MainActivity.kt
+++ app/src/main/java/com/example/basicworkmanager/MainActivity.kt
@@ -3,8 +3,18 @@ package com.example.basicworkmanager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import androidx.work.*
+import com.douglasstarnes.basicworkmanager.WorkStatusSingleton
+import com.example.basicworkmanager.databinding.ActivityMainBinding
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    //instance of the WorkManager
+    private val workManager = WorkManager.getInstance(this)
 
     lateinit var btnStartWork: Button
     lateinit var btnWorkStatus: Button
@@ -13,12 +23,48 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        btnStartWork = findViewById(R.id.btnStartWork)
-        btnWorkStatus = findViewById(R.id.btnWorkStatus)
-        btnResetStatus = findViewById(R.id.btnResetStatus)
-        btnWorkUIThread = findViewById(R.id.btnWorkUIThread)
+        btnStartWork = binding.btnStartWork
+        btnWorkStatus = binding.btnWorkStatus
+        btnResetStatus = binding.btnResetStatus
+        btnWorkUIThread = binding.btnWorkUIThread
+
+        btnStartWork.setOnClickListener {
+            //creating a one time work request
+            //Java
+//            val workRequest = OneTimeWorkRequest.Builder(SimpleWorker::class.java).build()
+
+            val data = workDataOf("WORK_MESSAGE" to "Work Completed!")
+
+            //Kotlin
+            val workRequest = OneTimeWorkRequestBuilder<SimpleWorker>()
+                    //input data to the worker
+                .setInputData(data)
+                .build()
+            //to repeat tasks periodically
+            val periodicWorkRequest = PeriodicWorkRequestBuilder<SimpleWorker>(
+                5, TimeUnit.MINUTES,
+            1, TimeUnit.MINUTES).build()
+            //enqueuing the workRequest in the workManager
+            workManager.enqueue(workRequest)
+        }
+
+        btnWorkStatus.setOnClickListener {
+            val toast = Toast.makeText(this, "The work status is: ${WorkStatusSingleton.workMessage}", Toast.LENGTH_SHORT)
+            toast.show()
+        }
+
+        btnResetStatus.setOnClickListener {
+            WorkStatusSingleton.workComplete = false
+        }
+
+        btnWorkUIThread.setOnClickListener {
+            Thread.sleep(10000)
+            WorkStatusSingleton.workComplete = true
+        }
 
     }
 }
