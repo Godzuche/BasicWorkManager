89b3dd9cccb5fed489af434d8ba492604b7a85d7
Administrator <godxuche@gmail.com>
2021-10-26 07:07:08 +0100

    created a simple Worker class

--- app/src/main/java/com/example/basicworkmanager/SimpleWorker.kt
+++ app/src/main/java/com/example/basicworkmanager/SimpleWorker.kt
@@ -7,8 +7,13 @@ import com.douglasstarnes.basicworkmanager.WorkStatusSingleton
 
 class SimpleWorker(context: Context, params: WorkerParameters): Worker(context, params) {
     override fun doWork(): Result {
+        val message = inputData.getString("WORK_MESSAGE")
         Thread.sleep(10000)
         WorkStatusSingleton.workComplete = true
+        if (message != null) {
+            WorkStatusSingleton.workMessage = message
+        } else
+            WorkStatusSingleton.workMessage = WorkStatusSingleton.workComplete.toString()
         return Result.success()
     }
 }
