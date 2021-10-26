c68e62aa730af87486b7670c66c76c2e8e531913
Administrator <godxuche@gmail.com>
2021-10-26 06:22:05 +0100

    finished setting up the basic  project...

--- app/src/main/java/com/example/basicworkmanager/MainActivity.kt
+++ app/src/main/java/com/example/basicworkmanager/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.basicworkmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnStartWork: Button
+    lateinit var btnWorkStatus: Button
+    lateinit var btnResetStatus: Button
+    lateinit var btnWorkUIThread: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnStartWork = findViewById(R.id.btnStartWork)
+        btnWorkStatus = findViewById(R.id.btnWorkStatus)
+        btnResetStatus = findViewById(R.id.btnResetStatus)
+        btnWorkUIThread = findViewById(R.id.btnWorkUIThread)
+
     }
 }
